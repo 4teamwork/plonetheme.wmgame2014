a9e7b659d280203ce824504f63cfac647d734c23
Thomas Buchberger <t.buchberger@4teamwork.ch>
2014-06-04 18:55:25 +0200

    Created new theme.

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0'
+
+setup(name='plonetheme.wmgame2014',
+      version=version,
+      description="WM Game 2014 Theme",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Thomas Buchberger',
+      author_email='t.buchberger@4teamwork.ch',
+      url='https://github.com/4teamwork/new-project',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
